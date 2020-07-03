38edcf9486a2270e555385e91ccbc62202ef05d0
Benjamin Kovács <kovacs.beni.1995@gmail.com>
2020-07-03 19:56:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     install_requires=[
         'cloudpickle>=1.2.1',
         'gym[atari,box2d,classic_control]>=0.15.3',
-        'ipython',
         'joblib',
         'matplotlib>=3.1.1',
         'mpi4py',
@@ -26,8 +25,6 @@ setup(
         'psutil',
         'scipy',
         'seaborn>=0.8.1',
-        'tensorflow>=1.8.0,<2.0',
-        'torch>=1.3.1,<2.0',
         'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
